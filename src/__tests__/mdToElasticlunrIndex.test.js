070625ad51ed4333ba5c94933700133fa6a5a58a
Michael Hsu (徐承志) <evenchange4@gmail.com>
2018-12-12 09:40:18 +0800

    update

--- src/__tests__/mdToElasticlunrIndex.test.js
+++ src/__tests__/mdToElasticlunrIndex.test.js
@@ -9,3 +9,10 @@ it('should return correct indexing', () => {
   const indexing = mdToElasticlunrIndex(filenames, identity);
   expect(indexing).toMatchSnapshot();
 });
+
+it('should return correct indexing with indexItemMapper', () => {
+  const filenames = glob.sync('./test-content/tutorial/*.md');
+  const indexItemMapper = (indexItem, filename) => ({ ...indexItem, filename });
+  const indexing = mdToElasticlunrIndex(filenames, indexItemMapper);
+  expect(indexing).toMatchSnapshot();
+});
