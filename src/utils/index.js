eab0733a271c66640c28e207c3543670bf2500d0
Michael Hsu (徐承志) <evenchange4@gmail.com>
2018-12-13 16:54:05 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,4 @@ export { default as concatBody } from './concatBody';
 export { default as defaultOptions } from './defaultOptions';
 export { default as extractIndexingFromMd } from './extractIndexingFromMd';
 export { default as remarkIndexing } from './remark-indexing';
+export { default as remarkLazysizes } from './remark-lazysizes';
